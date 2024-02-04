5b89de8e8809bbd5a33531f09f9994d03baa6bc5
LtARQ <qabdulraouf2@gmail.com>
2024-02-05 00:12:41 +0500

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ export const metadata: Metadata = {
     template: '%s | Acme Dashboard',
     default: 'Acme Dashboard',
   },
-  description: 'The official Next.js Course Dashboard, built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  description: 'The official Next.js Course Dashboard, built with App Router.'
 };
 
 export default function RootLayout({
